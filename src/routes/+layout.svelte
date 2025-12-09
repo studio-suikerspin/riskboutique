4ba2c715eaf4830dfabeeec0a65cdd4a52c5bcb2
Robin de Laater <robin.delaater@shopmonkey.nl>
2025-12-09 18:00:51 +0100

    Add favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,6 +21,13 @@
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 
+	<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<meta name="apple-mobile-web-app-title" content="Risk Boutique" />
+	<link rel="manifest" href="/site.webmanifest" />
+
 	<link rel="stylesheet" href="fonts/icons/iconly.css" />
 	<link rel="stylesheet" href="fonts/general-sans/general-sans.css" />
 </svelte:head>
