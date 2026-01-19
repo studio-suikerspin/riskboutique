ee079d1217e193753f1db4c6eea4d3a53a8ff2b3
sophieheijtlager <sophieheijtlager@gmail.com>
2026-01-19 20:34:51 +0100

    Start on slice

--- src/lib/slices/RevealTypeWithCta/index.svelte
+++ src/lib/slices/RevealTypeWithCta/index.svelte
@@ -1,12 +1,23 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client';
-	import type { SliceComponentProps } from '@prismicio/svelte';
+	import { PrismicLink, type SliceComponentProps } from '@prismicio/svelte';
 
 	type Props = SliceComponentProps<Content.ValuePropositionSlice>;
 
 	const { slice }: Props = $props();
 </script>
 
-<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+<section class="section-reveal-type" data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	RevealTypeWithCta
+	<PrismicLink class="btn btn--underline" field={slice.primary.cta_link} />
 </section>
+
+<style lang="scss">
+	.section-reveal-type {
+		padding: 32px;
+	}
+
+	.btn--underline {
+		background: transparent;
+	}
+</style>
