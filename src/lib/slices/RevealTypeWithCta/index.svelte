6a592d45302468eaf5142cbb94f2fa541e8ab749
sophieheijtlager <sophieheijtlager@gmail.com>
2026-01-19 21:31:45 +0100

    Continued working on RevealTypeWithCTA

--- src/lib/slices/RevealTypeWithCta/index.svelte
+++ src/lib/slices/RevealTypeWithCta/index.svelte
@@ -12,16 +12,27 @@
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
-	RevealTypeWithCta
-	<PrismicLink class="btn btn--underline" field={slice.primary.cta_link} />
+	<!-- Arrow right -->
+	<i class="icon-arrow-right"></i>
+
+	<!-- Text wrapper -->
+	<div class="reveal-type__text">
+		<!-- Label -->
+		<div>{slice.primary.label}</div>
+
+		<!-- Content -->
+		<div>{slice.primary.content}</div>
+
+		<!-- Link -->
+		<PrismicLink class="link" field={slice.primary.cta_link}>
+			{slice.primary.cta_link.text}
+			<i class="icon-arrow-right"></i>
+		</PrismicLink>
+	</div>
 </section>
 
 <style lang="scss">
 	.section-reveal-type {
 		padding: 32px;
-	}
-
-	:global .btn.btn--underline {
-		background: transparent;
 	}
 </style>
