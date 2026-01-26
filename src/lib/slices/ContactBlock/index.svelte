4eba9452c44e6f1ec1e4ae114973f9118b30131f
Robin de Laater <robin@suikerspin.studio>
2026-01-26 21:33:52 +0100

    Setup contact page and form

--- src/lib/slices/ContactBlock/index.svelte
+++ src/lib/slices/ContactBlock/index.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-	import type { Content } from '@prismicio/client'
-	import type { SliceComponentProps } from '@prismicio/svelte'
+	import Button from '$lib/components/Button.svelte'
+	import Label from '$lib/components/UI/Label.svelte'
+import type { Content } from '@prismicio/client'
+	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte'
 
 	type Props = SliceComponentProps<Content.ContactBlockSlice>
 
@@ -10,13 +12,196 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
+	class="contact-block block-padding"
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+    <div class="contact-block__container">
+        <div class="contact-block__inner">
+            <div class="content-wrap">
+                <div class="content-wrap__group">
+                   <Label>
+                        {slice.primary.label_text}
+                   </Label>
+
+                    <h1 class="h3 contact-title">{slice.primary.heading}</h1>
+
+                    <div class="contact-subtitle">
+                        <PrismicRichText field={slice.primary.subtitle} />
+                    </div>
+                </div>
+
+                <div class="content-wrap__group">
+                    <h4>Get in touch</h4>
+                    <ul class="contact-methods">
+                        {#each slice.primary.contact_methods as item, index (index)}
+                            <li class="contact-methods__item">
+                                <strong>{item.label}:</strong> <PrismicRichText field={item.content} />
+                            </li>
+                        {/each}
+                    </ul>
+                </div>
+            </div>
+
+            <div class="contact-block__form-wrap">
+                <div class="form-header">
+                    <h3 class="form-header__title h3">{slice.primary.form_heading}</h3>
+                    <div class="form-header__description">
+                        <PrismicRichText field={slice.primary.form_description} />
+                    </div>
+                </div>
+                <form action="" class="contact-form">
+                    <div class="contact-form__group">
+                        <label for="name" class="contact-form__label">Name</label>
+                        <input type="text" id="name" class="contact-form__input" placeholder="Your name" required />
+                    </div>
+                    <div class="contact-form__group">
+                        <label for="email" class="contact-form__label">Email</label>
+                        <input type="email" id="email" class="contact-form__input" placeholder="Your email" required />
+                    </div>
+                    <div class="contact-form__group">
+                        <label for="phone" class="contact-form__label">Phone</label>
+                        <input type="tel" id="phone" class="contact-form__input" placeholder="Your phone number" required />
+                    </div>
+                    <div class="contact-form__group">
+                        <label for="message" class="contact-form__label">Message</label>
+                        <textarea id="message" class="contact-form__textarea" placeholder="Your message" required></textarea>
+                    </div>
+
+                    <div class="contact-form__button-wrap">
+                        <Button className="contact-form__button" variant="white" text={slice.primary.form_button_text || 'Submit'} icon="icon-arrow-right" type="submit" />
+
+                        <div class="contact-form__footer-note">
+                            <PrismicRichText field={slice.primary.form_footer_note} />
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
+
+<style lang="scss">
+    .contact-block {
+        &__container {
+            max-width: var(--container-width);
+            margin-inline: auto;
+
+            padding-inline: var(--container-padding);
+        }
+
+        &__inner {
+            display: flex;
+            flex-flow: column;
+            gap: 2.5rem;
+
+            @media (min-width: 1024px) {
+                flex-flow: row;
+            }
+        }
+
+        .content-wrap {
+            flex: 1;
+            display: flex;
+            flex-flow: column;
+            gap: 2.5rem;
+
+            &__group {
+                display: flex;
+                flex-flow: column;
+                gap: 1.5rem;
+            }
+        }
+
+        .contact-subtitle {
+            font-size: 1rem;
+            font-weight: 500;
+            line-height: 1.5;
+            color: var(--color-dark-mode);
+        }
+
+        .contact-methods {
+            display: flex;
+            flex-flow: column;
+            gap: 1rem;
+
+            &__item {
+                display: flex;
+                gap: 0.5rem;
+            }
+        }
+
+        &__form-wrap {
+            flex: 1;
+            padding-block: 2.5rem;
+            padding-inline: 1.5rem;
+
+            border-radius: 0.5rem;
+
+            background: var(--color-dark-mode);
+            color: var(--color-snow-white);
+
+            display: flex;
+            flex-flow: column;
+            gap: 2.5rem;
+
+            .form-header {
+                display: flex;
+                flex-flow: column;
+                gap: 1.5rem;
+            }
+
+            .contact-form {
+                display: flex;
+                flex-flow: column;
+                gap: 1.5rem;
+
+                &__group {
+                    display: flex;
+                    flex-flow: column;
+                    gap: 0.5rem;
+                }
+
+                &__label {
+                    font-size: 1.125rem;
+                    font-weight: 400;
+                }
+
+                &__input {
+                    padding: 1rem;
+                    border-radius: 0.25rem;
+
+                    width: 100%;
+                    height: 2.5rem;
+
+                    background: rgba(249, 249, 248, 0.20);
+
+                    &::placeholder {
+                        color: var(--color-snow-white);
+                        opacity: 0.7;
+                        font-weight: 400;
+                        font-size: 1rem;
+                    }
+                }
+
+                &__textarea {
+                    padding: 1rem;
+                    border-radius: 0.25rem;
+                    background: rgba(249, 249, 248, 0.20);
+                }
+
+                &__button-wrap {
+                    display: flex;
+                    flex-flow: column;
+                    gap: 1rem;
+                }
+
+                :global &__button {
+                    width: 100%;
+                }
+
+                &__footer-note {
+                    font-size: 1.125rem;
+                }
+            }
+        }
+    }
+</style>
