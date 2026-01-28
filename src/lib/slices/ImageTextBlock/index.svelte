c6ac993983d657c3f941dc23a609b8f99afc1aac
sophieheijtlager <sophieheijtlager@gmail.com>
2026-01-28 21:02:28 +0100

    Started working on image text blocks for careers page

--- src/lib/slices/ImageTextBlock/index.svelte
+++ src/lib/slices/ImageTextBlock/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client'
-	import type { SliceComponentProps } from '@prismicio/svelte'
+	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte'
 
 	type Props = SliceComponentProps<Content.ImageTextColumns2Slice>
 
@@ -8,15 +8,58 @@
 </script>
 
 <section
+	class="section-careers-image-block"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
+	data-id={slice.id}
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+	<img
+		src={slice.primary.media.url}
+		alt={slice.primary.media.alt}
+	/>
+
+	<div class="section-careers-image-block__content">
+		<h4>{slice.primary.headline}</h4>
+		<PrismicRichText field={slice.primary.description} />
+	</div>
 </section>
+
+<style lang="scss">
+	.section-careers-image-block {
+		padding-inline: 0.75rem;
+		display: flex;
+		flex-direction: column;
+		gap: 2.5rem;
+
+		&[data-id='image_text_columns_2$c10ad0eb-ce83-405f-9fa8-6587935358b2'] {
+			padding-top: 5rem;
+		}
+
+		@media (min-width: 992px) {
+			padding-inline: 1.25rem;
+			flex-direction: row;
+			gap: 5rem;
+		}
+
+		& img {
+			width: 100%;
+			order: 1;
+
+			@media (min-width: 992px) {
+				width: 50%;
+				order: -1;
+			}
+		}
+
+		&__content {
+			display: flex;
+			flex-direction: column;
+			gap: 1.5rem;
+
+			:global & ul {
+				padding-inline-start: 1.5rem;
+				list-style: unset;
+			}
+		}
+	}
+</style>
