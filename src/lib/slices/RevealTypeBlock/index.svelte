5e316eb02a65b6a297519286fecc7169ee819afa
sophieheijtlager <sophieheijtlager@gmail.com>
2026-01-28 20:24:48 +0100

    start on reveal type section careers page

--- src/lib/slices/RevealTypeBlock/index.svelte
+++ src/lib/slices/RevealTypeBlock/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+	import RevealTypeContent from '$lib/components/RevealTypeContent.svelte'
 	import type { Content } from '@prismicio/client'
-	import type { SliceComponentProps } from '@prismicio/svelte'
 
 	type Props = SliceComponentProps<Content.HighlightedTextBlockSlice>
 
@@ -8,15 +8,15 @@
 </script>
 
 <section
+	class="section-careers-reveal-type"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+	<RevealTypeContent text={slice.primary.content} />
 </section>
+
+<style>
+	.section-careers-reveal-type {
+		height: 1000px;
+	}
+</style>
