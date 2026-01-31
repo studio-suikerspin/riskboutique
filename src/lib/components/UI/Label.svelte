746b0ba3289c76724c2b7ed5d0d9e2db361833c6
Robin de Laater <robin@suikerspin.studio>
2026-01-31 11:15:42 +0100

    Contact page (#20)

---
+++ src/lib/components/UI/Label.svelte
@@ -0,0 +1,28 @@
+<script>
+    let { children } = $props();
+</script>
+
+<div class="label">
+    {@render children()}
+</div>
+
+<style lang="scss">
+    .label {
+        display: flex;
+        padding: 0.75rem 1.5rem;
+        justify-content: center;
+        align-items: center;
+        gap: 0.625rem;
+
+        width: fit-content;
+
+        border-radius: 8px 0;
+
+        font-size: 1.125rem;
+        font-weight: 600;
+        color: var(--color-snow-white);
+
+        background: url('bg-gradient-donker.svg') lightgray 50% / cover top-left no-repeat;
+        background-color: var(--color-aqua);
+    }
+</style>
