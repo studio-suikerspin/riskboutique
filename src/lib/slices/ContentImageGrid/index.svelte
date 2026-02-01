8c758e34f813ea85a44ce3068dfb9a0914722bdb
Robin de Laater <robin@suikerspin.studio>
2026-02-01 13:21:32 +0100

    feat: create content grid

--- src/lib/slices/ContentImageGrid/index.svelte
+++ src/lib/slices/ContentImageGrid/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client'
-	import type { SliceComponentProps } from '@prismicio/svelte'
+	import { PrismicRichText, PrismicImage, type SliceComponentProps } from '@prismicio/svelte'
 
 	type Props = SliceComponentProps<Content.ContentImageGridSlice>
 
@@ -8,15 +8,41 @@
 </script>
 
 <section
+	class="content-grid block-padding"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+     <div class="container">
+          <div class="content-grid__grid">
+               {#each slice.primary.items as item, index (index)}
+                    {console.log(item)}
+                    <div class="grid-item grid-item--{item.type}">
+                         {#if item.type === 'text'}
+                              <div class="grid-item__text general-content">
+                                   <PrismicRichText field={item.text} />
+                              </div>
+                         {:else if item.type === 'image'}
+                              <PrismicImage field={item.image} class="grid-item__image" />
+                         {/if}
+                    </div>
+               {/each}
+          </div>
+     </div>
 </section>
+
+<style lang="scss">
+.content-grid {
+     &__grid {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(30%, 1fr));
+          grid-template-rows: repeat(3, 1fr);
+          column-gap: 4.5rem;
+     }
+
+     .grid-item {
+          &--image {
+               grid-row: span 2;
+          }
+     }
+}
+</style>
