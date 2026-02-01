aaf4b1d30f19cf5293fe15e692bf01cacd0278b0
Jesse van Loon <jesse@suikerspin.studio>
2026-02-01 16:13:20 +0100

    small change advisory

--- src/prismicio-types.d.ts
+++ src/prismicio-types.d.ts
@@ -70,6 +70,7 @@ type ContentRelationshipFieldWithData<
 }[Exclude<TCustomType[number], string>['id']]
 
 type AdvisorySubpageDocumentDataSlicesSlice =
+	| ImageTextColumns2Slice
 	| HeroWithBackgroundSlice
 	| MarqueeTextSlice
 	| FeatureGrid2Slice
