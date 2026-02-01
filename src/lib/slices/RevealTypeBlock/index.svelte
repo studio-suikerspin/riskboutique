11a38d4392b98e926fe317cf0ef87f4e5e754307
Robin de Laater <robin@suikerspin.studio>
2026-02-01 20:03:49 +0100

    basic reveal type block

--- src/lib/slices/RevealTypeBlock/index.svelte
+++ src/lib/slices/RevealTypeBlock/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import RevealTypeContent from '$lib/components/RevealTypeContent.svelte'
 	import type { Content } from '@prismicio/client'
 	import type { SliceComponentProps } from '@prismicio/svelte'
 
@@ -8,15 +9,11 @@
 </script>
 
 <section
+     class="block-padding-{slice.primary.section_padding} {slice.primary.section_padding === 'both' ? 'block-padding' : ''}"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+     <div class="container">
+          <RevealTypeContent text={slice.primary.content} />
+     </div>
 </section>
