d0f03f9a984eb24f663f6e7643b2705712a4844f
Jesse van Loon <jesse@suikerspin.studio>
2026-02-01 18:43:24 +0100

    changed image text and careers hero

--- src/lib/slices/ImageTextBlock/index.svelte
+++ src/lib/slices/ImageTextBlock/index.svelte
@@ -1,57 +1,88 @@
 <script lang="ts">
 	import type { Content } from '@prismicio/client'
 	import { PrismicRichText, type SliceComponentProps } from '@prismicio/svelte'
+	import { PrismicImage } from '@prismicio/svelte'
+	import { initContentRevealScroll } from '$lib/revealContent.js'
+	import { onMount } from 'svelte'
 
 	type Props = SliceComponentProps<Content.ImageTextColumns2Slice>
 
 	const { slice }: Props = $props()
+
+	onMount(() => {
+    initContentRevealScroll()
+  })
+
+	const paddingClass = $derived(() => {
+		switch (slice.primary.section_padding) {
+			case 'top': return 'block-padding-top';
+			case 'bottom': return 'block-padding-bottom';
+			case 'both': return 'block-padding';
+			case 'none': return '';
+			default: return '';
+		}
+	});
 </script>
 
 <section
-	class="section-careers-image-block"
+	class="image-block {paddingClass()}"
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
 	data-id={slice.id}
 >
-	<img
-		src={slice.primary.media.url}
-		alt={slice.primary.media.alt}
-	/>
 
-	<div class="section-careers-image-block__content">
-		<h4>{slice.primary.headline}</h4>
-		<div>
-			<PrismicRichText field={slice.primary.description} />
+	<div class="container">
+		<div class="image-block__inner">
+		{#each slice.primary.image_text_block as item}
+			<div class="image-block__item" data-reveal-group>
+				<div class="image-block__image">
+					<PrismicImage field={item.image} />
+				</div>
+				<div class="image-block__content">
+					<h4>{item.headline}</h4>
+					<div class="rich-text-content">
+						<PrismicRichText field={item.description} />
+					</div>
+				</div>
+			</div>
+		{/each}
 		</div>
 	</div>
 </section>
 
 <style lang="scss">
-	.section-careers-image-block {
-		padding-inline: 0.75rem;
-		display: flex;
-		flex-direction: column;
-		gap: 2.5rem;
-		max-width: 1500px;
-		margin: auto;
+	.image-block {
 
-		&[data-id='image_text_columns_2$c10ad0eb-ce83-405f-9fa8-6587935358b2'] {
-			padding-top: 5rem;
-		}
-
-		@media (min-width: 992px) {
-			padding-inline: 1.25rem;
-			flex-direction: row;
+		&__inner{
+			display: flex;
+			flex-direction: column;
 			gap: 5rem;
 		}
 
-		& img {
-			width: 100%;
-			order: 1;
+		&__item{
+			display: flex;
+			flex-direction: column;
+			gap: 2rem;
 
-			@media (min-width: 992px) {
-				width: 50%;
-				order: -1;
+			@media(min-width: 992px){
+				gap: 5rem;
+				flex-direction: row;
+			}
+		}
+
+		&__image {
+			flex: 3;
+			max-height: 750px;
+			overflow: hidden;
+			border-radius: 0.5rem;
+
+			:global img {
+				width: 100%;
+				height: 100%;
+				display: block;
+				object-fit: cover;
+				max-height: 100%;
+				max-width: 100%;
 			}
 		}
 
@@ -59,10 +90,16 @@
 			display: flex;
 			flex-direction: column;
 			gap: 1.5rem;
+			// max-width: 46rem;
+			flex: 4;
 
 			:global & ul {
 				padding-inline-start: 1.5rem;
 				list-style: unset;
+			}
+
+			.rich-text-content {
+				max-width: 46rem;
 			}
 		}
 	}
