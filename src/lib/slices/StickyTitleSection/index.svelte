af42f9b124a600c7cb80c03d8502eeabca65ad09
sophieheijtlager <sophieheijtlager@gmail.com>
2026-02-01 16:53:23 +0100

    fixed css for sticky title section

--- src/lib/slices/StickyTitleSection/index.svelte
+++ src/lib/slices/StickyTitleSection/index.svelte
@@ -106,53 +106,48 @@
 <style lang="scss">
 	.section-sticky-title {
 		background: linear-gradient(#0b0e13, #0e4f63);
+		color: #f9f9f8;
 		width: 100%;
 		height: 350vh;
 		position: relative;
-		color: #f9f9f8;
 
 		&__container {
-			display: flex;
 			justify-content: center;
 			align-items: center;
 			width: 100%;
 			height: 100vh;
-			padding-left: 1.5em;
-			padding-right: 1.5em;
-
+			padding-inline: 2rem;
+			display: flex;
 			position: sticky;
 			top: 0;
 		}
 
 		&__inner {
-			display: flex;
+			text-align: center;
 			flex-flow: column;
 			justify-content: center;
 			align-items: center;
 			width: 100%;
-
-			max-width: 60em;
+			max-width: 1200px;
+			margin-inline: auto;
+			display: flex;
 			position: relative;
-			text-align: center;
-
-			margin-left: auto;
-			margin-right: auto;
 		}
 
 		&__el {
-			margin-block-start: 0;
-			margin-block-end: 0;
-
-			font-size: 2rem;
+			margin-top: 0;
+			margin-bottom: 0;
+			font-size: 4rem;
 			font-weight: 500;
+			line-height: 1;
 
-			.is--stacked {
+			&.is--stacked {
 				visibility: hidden;
 				position: absolute;
 			}
 
-			@media (min-width: 992px) {
-				font-size: 4rem;
+			@media (max-width: 767px) {
+				font-size: 2rem;
 			}
 		}
 	}
