2a01d0af949a0ca54d581280270b059b7e817390
Jesse van Loon <jesse@suikerspin.studio>
2026-02-01 12:41:08 +0100

    final hero background

--- src/lib/slices/HeroWithBackground/index.svelte
+++ src/lib/slices/HeroWithBackground/index.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte'
 	import type { Content } from '@prismicio/client'
 	import type { SliceComponentProps } from '@prismicio/svelte'
+	import { PrismicImage, PrismicLink } from '@prismicio/svelte'
+	import { initContentRevealScroll } from '$lib/revealContent.js'
+	import { onMount } from "svelte"
 
 	type Props = SliceComponentProps<Content.HeroWithBackgroundSlice>
+
+	onMount(() => {
+		initContentRevealScroll()
+	})
 
 	const { slice }: Props = $props()
 </script>
@@ -10,13 +18,112 @@
 <section
 	data-slice-type={slice.slice_type}
 	data-slice-variation={slice.variation}
+	class="banner block-padding-bottom"
 >
-	Placeholder component for {slice.slice_type} (variation: {slice.variation})
-	slices.
-	<br />
-	<strong>You can edit this slice directly in your code editor.</strong>
-	<!--
-	💡 Use the Prismic MCP server with your code editor
-	📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-	-->
+	<div class="container">
+		<div class="banner__inner">
+			<div class="banner__image">
+				<PrismicImage field={slice.primary.background_image} />
+				<div class="banner__overlay"></div>
+			</div>
+			<div class="banner__text" data-reveal-group>
+				<div class="h2">{slice.primary.headline}</div>
+				<div class="h3 low">{slice.primary.headline}</div>
+				<div class="banner__buttons">
+					{#each slice.primary.cta_button as link (link.key)}
+						<Button
+							field={link}
+							text={link.text || 'Connect with us'}
+							variant={link.variant || 'light'}
+						/>
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
 </section>
+
+<style lang="scss">
+	.banner{
+
+		&__inner{
+			position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			text-align: center;
+			min-height: 20rem;
+			padding: 1.5rem;
+			overflow: hidden;
+			border-radius: 0.5rem;
+
+			.h3{
+				display: block;
+
+				@media(min-width: 768px){
+					display: none;
+				}
+			}
+
+			.h2{
+				display: none;
+				@media(min-width: 768px){
+					display: block;
+				}
+			}
+			
+			
+
+			@media(min-width: 768px){
+				min-height: 36.625rem;
+				padding: 2.5rem;
+				border-radius: 1.5rem 0;
+			}
+		}
+
+		&__image{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			overflow: hidden;
+			z-index: 1;
+
+			img{
+				width: 100%;
+				height: 100%;
+				object-fit: cover;
+			}
+		}
+
+		&__overlay{
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 100%;
+			height: 100%;
+			background: rgba(0, 0, 0, 0.5);
+			z-index: 1;
+		}
+
+		&__text{
+			color: var(--color-snow-white);
+			z-index: 2;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			gap: 2.5rem;
+		}
+
+		&__buttons{
+			display: flex;
+			gap: 0.5rem;
+
+			:global .btn--secondary{
+				background: transparent;
+				color: var(--color-snow-white);
+			}
+		}
+	}
+</style>
