9c480a577129c5e14d5768f644d9e4c739cfde6f
Robin de Laater <robin@suikerspin.studio>
2026-02-01 15:37:47 +0100

    feat: use mailto in contact endpoint

--- src/routes/api/contact/+server.ts
+++ src/routes/api/contact/+server.ts
@@ -1,33 +1,43 @@
 import { json } from '@sveltejs/kit'
 import { Resend } from 'resend'
 import { RESEND_API_KEY, RESEND_TEMPLATE_ID } from '$env/static/private'
+import { createClient } from '$lib/prismicio.js'
 
 const resend = new Resend(RESEND_API_KEY)
 
-export async function POST({ request }) {
+export async function POST({ request, fetch }) {
 	const { name, email, phone, message } = await request.json()
 
-    if (! name || ! email || ! phone || ! message) {
-        return json({ success: false, error: 'Missing required fields' }, { status: 400 })
-    }
+	if (!name || !email || !phone || !message) {
+		return json(
+			{ success: false, error: 'Missing required fields' },
+			{ status: 400 }
+		)
+	}
 
-    try {
-        const { data, error } = await resend.emails.send({
-            from: 'Risk Boutique <riskboutique@suikerspin.studio>',
-            to: ['robin@suikerspin.studio'],
-            template: {
-                id: RESEND_TEMPLATE_ID,
-                variables: { name, email, phone, message }
-            }
-        })
+	const client = createClient({ fetch })
+	const siteSettings = await client.getSingle('site_settings')
+	const mailTo = siteSettings.data.contact_form_mails.map((item) =>
+		item.email?.toString()
+	).filter((item) => item !== undefined)
 
-        if (error) {
-            console.log(error)
-            return json({ success: false, error }, { status: 500 })
-        }
+	try {
+		const { data, error } = await resend.emails.send({
+			from: 'Risk Boutique <riskboutique@suikerspin.studio>',
+			to: mailTo.length ? [...mailTo] : ['info@suikerspin.studio'],
+			template: {
+				id: RESEND_TEMPLATE_ID,
+				variables: { name, email, phone, message }
+			}
+		})
 
-        return json({ success: true, data })
-    } catch (error) {
-        return json({ success: false, error }, { status: 500 })
-    }
+		if (error) {
+			console.log(error)
+			return json({ success: false, error }, { status: 500 })
+		}
+
+		return json({ success: true, data })
+	} catch (error) {
+		return json({ success: false, error }, { status: 500 })
+	}
 }
