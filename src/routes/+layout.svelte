4380e094bee7af9ae16ca72a9c9438a31c937cbe
Robin de Laater <robin@suikerspin.studio>
2026-02-02 20:51:14 +0100

    feat: basic overflowing image hero

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,13 +13,13 @@
 	let { children } = $props()
 
 	onNavigate((navigation) => {
-	    if (!document.startViewTransition) return;
+		if (!document.startViewTransition) return
 
 		return new Promise((resolve) => {
-		    document.startViewTransition(async () => {
-				resolve();
-				await navigation.complete;
-		    })
+			document.startViewTransition(async () => {
+				resolve()
+				await navigation.complete
+			})
 		})
 	})
 </script>
