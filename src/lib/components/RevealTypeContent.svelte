d0cfde61be04eaaa92dc3f7c6a2ffe1f54fe92de
Robin de Laater <robin@suikerspin.studio>
2026-02-02 20:18:41 +0100

    feat: update revealType block to allow stacked content

--- src/lib/components/RevealTypeContent.svelte
+++ src/lib/components/RevealTypeContent.svelte
@@ -2,46 +2,54 @@
 <!-- <RevealTypeContent text={slice.primary.content} />-->
 
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { browser } from '$app/environment';
-	import { gsap } from '$lib/gsap';
-	import { SplitText } from 'gsap/SplitText';
-	import type { KeyTextField } from '@prismicio/client';
+	import { onMount } from 'svelte'
+	import { browser } from '$app/environment'
+	import { gsap } from '$lib/gsap'
+	import { SplitText } from 'gsap/SplitText'
+	import type { KeyTextField } from '@prismicio/client'
 
 	type Props = {
-		text: KeyTextField;
-		darkBackground?: boolean;
-	};
+		text: KeyTextField
+		darkBackground?: boolean
+		trigger?: string | HTMLElement | null
+	}
 
-	const { text, darkBackground = false }: Props = $props();
-	let textElement: HTMLElement;
+	const { text, darkBackground = false, trigger }: Props = $props()
+	let textElement: HTMLElement
 
 	function revealType() {
-		if (!textElement) return;
+		if (!textElement) return
 
-		let split = SplitText.create(textElement, { type: 'words' });
+		let split = SplitText.create(textElement, { type: 'words' })
 
 		const tl = gsap.timeline({
 			scrollTrigger: {
-				trigger: textElement,
+				trigger: trigger || textElement,
 				start: `top top+=${800}`, // Start when the top of the elment hits the top of the container + 100px
 				end: `center top`, // End when the center of the element hits the top of the container
 				scrub: true
 				// markers: true
 			}
-		});
+		})
 
 		tl.from(split.words, {
-			color: darkBackground ? 'rgba(249, 249, 248, 0.10)' : 'rgba(0, 0, 0, 0.08)',
+			color: darkBackground
+				? 'rgba(249, 249, 248, 0.10)'
+				: 'rgba(0, 0, 0, 0.08)',
 			stagger: 0.1
-		});
+		})
 	}
 
 	onMount(() => {
 		if (browser) {
-			revealType();
+			revealType()
 		}
-	});
+	})
 </script>
 
-<h3 class="reveal-type-content" bind:this={textElement}>{text}</h3>
+<h3
+	class="reveal-type-content"
+	bind:this={textElement}
+>
+	{text}
+</h3>
