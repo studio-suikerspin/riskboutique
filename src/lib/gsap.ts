80f337a645e4ac3690d042db639fa43064545e04
Robin de Laater <robin@suikerspin.studio>
2026-02-05 21:14:45 +0100

    fix: grid spacing and correct importing

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -4,9 +4,10 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollSmoother } from 'gsap/ScrollSmoother'
 import { CustomEase } from 'gsap/CustomEase'
 import { SplitText } from 'gsap/dist/SplitText'
+import { IntertiaPlugin } from 'gsap/InertiaPlugin'
 
 if (browser) {
-	gsap.registerPlugin(ScrollTrigger, ScrollSmoother, CustomEase, SplitText)
+	gsap.registerPlugin(ScrollTrigger, ScrollSmoother, CustomEase, SplitText, InertiaPlugin)
 }
 
-export { gsap, ScrollTrigger, ScrollSmoother, CustomEase, SplitText }
+export { gsap, ScrollTrigger, ScrollSmoother, CustomEase, SplitText, IntertiaPlugin }
