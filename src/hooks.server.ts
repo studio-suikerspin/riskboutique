14e9b6e469e5fe3bd6b14521f27e239f2ae84ca7
Robin de Laater <robin@suikerspin.studio>
2026-02-17 18:32:17 +0100

    setting cache control header

--- src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-export async function handle({ event, resolve }) {
-	const response = await resolve(event);
-
-	if (response.headers.get('content-type')?.includes('text/html')) {
-		response.headers.set('Cache-Control', 'public, s-maxage=60');
-	}
-
-	return response;
-}
