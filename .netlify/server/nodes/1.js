8cf541c6c072348f9624e97cf7045146801f4f1e
Robin de Laater <robin@suikerspin.studio>
2026-02-21 15:09:08 +0100

    chore: remove netlify stuff

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DM06PQdA.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/B1Ul0HW3.js","_app/immutable/chunks/BGm2FRE4.js","_app/immutable/chunks/C5wg99Yo.js","_app/immutable/chunks/CNehfJ4j.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .netlify/server/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/slice-simulator/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.D5TxgMMk.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/B1Ul0HW3.js","_app/immutable/chunks/BGm2FRE4.js","_app/immutable/chunks/DkMiz2ez.js","_app/immutable/chunks/DO6RCpIk.js","_app/immutable/chunks/Cp6hzOhC.js","_app/immutable/chunks/DZizjJuI.js"];
-export const stylesheets = ["_app/immutable/assets/index.DR4GlrGl.css"];
-export const fonts = [];
