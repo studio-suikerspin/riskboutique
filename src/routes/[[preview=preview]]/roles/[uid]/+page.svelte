0b1c37346fab81d56c8209f2bb543120408a1351
Robin de Laater <robin@suikerspin.studio>
2026-07-03 15:21:39 +0200

    feat: add contact avatar to role page

--- src/routes/[[preview=preview]]/roles/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/roles/[uid]/+page.svelte
@@ -6,6 +6,8 @@
 	const { data } = $props()
 
 	const role = data.role || []
+
+	console.log(role.data.contact_avatar)
 </script>
 
 <svelte:head>
@@ -28,6 +30,17 @@
 			<div class="role__content rich-text-content">
 				<PrismicRichText field={role.data.content} />
 			</div>
+
+			{#if role.data.contact_avatar}
+				<AvatarWithContactInfo
+					image={role.data.contact_avatar.data.image}
+					name={role.data.contact_avatar.data.name}
+					role={role.data.contact_avatar.data.role}
+					phone={role.data.contact_avatar.data.phone}
+					email={role.data.contact_avatar.data.email}
+					second_role={role.data.contact_avatar.data.second_role}
+				/>
+			{/if}
 
 			<div class="role__cta">
 				<a
